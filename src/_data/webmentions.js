5783df41daba670fba5cd71679c585dab2e7b30f
reddmo <126845456+reddmo@users.noreply.github.com>
2024-02-08 21:50:51 -0500

    Update webmentions.js

--- src/_data/webmentions.js
+++ src/_data/webmentions.js
@@ -1,5 +1,6 @@
+// Fetch webmentions from webmention.io API
 const EleventyFetch = require("@11ty/eleventy-fetch");
-require('dotenv').config()
+
 module.exports = async function () {
   const WEBMENTIONS_STUFF = process.env.WEBMENTION_IO_TOKEN;
   const url = `https://webmention.io/api/mentions.jf2?token=${WEBMENTIONS_STUFF}&per-page=900`;
@@ -8,6 +9,7 @@ module.exports = async function () {
     type: "json",
   });
   const webmentions = await res;
+  // console.log(JSON.stringify(webmentions, null, 2));
   return {
     mentions: webmentions.children,
   };
